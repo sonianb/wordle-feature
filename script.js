452eda8bc2dbed80c5b464e9537519d2d76e25c5
sonianb <sonia.botsarova@gmail.com>
2022-01-31 21:17:49 +0000

    function to check the letter

--- script.js
+++ script.js
@@ -18,3 +18,14 @@ const words = [
   }
   
   console.log(generateWord(words));
+
+  //function that will determine if it the letter is correct and at the right position, misplaced or not correct
+function checkLetter(word, letter, index) {
+  if(word.charAt(index) === letter) {
+    return 'Well done! The index is at the correct position.'
+  }
+  if(word.includes(letter)) {
+    return 'The word contains this letter';
+  } 
+  return 'Wrong letter and position';
+} 
