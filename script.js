dd97be839376d4eef455675b0ed959c5b0c5fc9a
sonianb <sonia.botsarova@gmail.com>
2022-02-01 12:46:01 +0000

    add comments and improve logic structure

--- script.js
+++ script.js
@@ -1,51 +1,56 @@
-const boxOne = document.getElementById('box1');
+// *************************
+// * Game global variables *
+// *************************
 
 const words = [
-    'apple',
-    'chair',
-    'dream',
-    'drive',
-    'floor',
-    'pilot',
-    'reply',
-    'staff',
-    'thing',
-    'woman',
-  ];
-  
-  
-  //function that will generate a random word from the words array
-  function generateWord(words) {
-    return words[Math.floor(Math.random() * words.length)];
-  }
-  
-  //store the word in a vairable
-  let word = generateWord(words)
-  console.log(word);
+  'apple',
+  'chair',
+  'dream',
+  'drive',
+  'floor',
+  'pilot',
+  'reply',
+  'staff',
+  'thing',
+  'woman',
+];
 
-  //function that will determine if it the letter is correct and at the right position, misplaced or not correct
+//store the answer in a variable
+const answer = generateWord(words)
+console.log(answer);
+
+//store each letter of the word the user is testing
+let userWord = [];
+
+// **************
+// * Game logic *
+// **************
+
+//function that will generate a random word from the words array
+function generateWord(words) {
+  return words[Math.floor(Math.random() * words.length)];
+}
+
+//function that will determine if it the letter is correct and at the right position, misplaced or not correct
 function checkLetter(word, letter, index) {
-  if(word.charAt(index) === letter) {
+  if (word.charAt(index) === letter) {
     return 'Well done! The index is at the correct position.'
   }
-  if(word.includes(letter)) {
+  if (word.includes(letter)) {
     return 'The word contains this letter';
-  } 
+  }
   return 'Wrong letter and position';
-} 
+}
+
+// ********************
+// * Dom manipulation *
+// ********************
+
 
 document.addEventListener('keypress', (event) => {
   let letter = event.key;
   console.log(letter);
   boxOne.innerText = letter;
-
 });
 
 
-
-// input.addEventListener('input', (event)=> {
-//   let letter = event.data;
-//   let result =  checkLetter(word, letter, index);
-//   console.log(result);
-// }
-// );
